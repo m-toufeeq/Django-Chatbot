63a712052bd7b161ebeda80db7bfa1215c04bfa4
Toufeeq <toufeeqsemi111@gmail.com>
2024-08-27 11:45:09 +0500

    added url of the homepage

--- Chatbot/ChabotFeature/urls.py
+++ Chatbot/ChabotFeature/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Renders the index.html at the root URL
+    path('homepage', views.homepage, name='homepage'),  
+    path('index', views.index, name='index'),  
     path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='ChabotFeature/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
